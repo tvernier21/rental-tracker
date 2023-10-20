399e7b6f940d0f9a8d8ece62d21cce8863d5276c
tvernier21 <thibaultvernier13@gmail.com>
2023-10-20 15:10:16 -0400

    loading state fixes multiselect bug

--- app/components/SidebarNav.tsx
+++ app/components/SidebarNav.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import "@/app/(root)/(routes)/properties/[propertyId]/box.css";
-import { useState } from "react"
-import { useRouter } from "next/navigation"
+import { useState, useEffect } from "react"
+import { useRouter, usePathname } from "next/navigation"
 import {
     Listbox,
     ListboxItem,
-    ListboxSection
+    ListboxSection,
+    Spinner
 } from "@nextui-org/react"
 
 interface SidebarNavProps {
@@ -22,51 +23,70 @@ const SidebarNav: React.FC<SidebarNavProps> = ({
     items 
 }) => {
     const router = useRouter();
+    const pathname = usePathname();
+    const [isLoading, setIsLoading] = useState(true);
     const [selectedKeys, setSelectedKeys] = useState(new Set([items[0].key]));
-    console.log(selectedKeys)
+
+    useEffect(() => {
+        console.log(selectedKeys);
+        if (isLoading) {
+            const item = items.find(item => item.href === pathname);
+            if (item) {
+                setSelectedKeys(new Set([item.key]));
+            } else {
+                setSelectedKeys(new Set([items[0].key]));
+            }
+            setIsLoading(false);
+        }
+    }, [pathname, isLoading, items]);
+
 
     return (
         <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
             <div className="w-full max-w-[360px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
-                <Listbox
-                    aria-label="Dynamic Actions"
-                    variant="solid"
-                    disallowEmptySelection={true}
-                    selectionMode="single"
-                    selectedKeys={selectedKeys}
-                    onSelectionChange={(keys) => {
-                        if (keys instanceof Set) {
-                            setSelectedKeys(keys as Set<string>);
-                        } else {
-                            setSelectedKeys(new Set([keys]));
-                        }
-                    }}
-                    autoFocus={true}
-                    itemClasses={{
-                        selectedIcon: "hide-icon"
-                    }}
-                >
-                    <ListboxSection title="General" showDivider>
-                        {items.filter(item => item.section === "general").map(item => (
-                            <ListboxItem
-                                key={item.key}
-                                onPress={() => router.push(item.href)}
-                            >
-                                {item.title}
-                            </ListboxItem>
-                        ))}
-                    </ListboxSection>
-                    <ListboxSection title="Contracts">
-                        {items.filter(item => item.section === "contracts").map(item => (
-                            <ListboxItem
-                                key={item.key}
-                                onPress={() => router.push(item.href)}
-                            >
-                                {item.title}
-                            </ListboxItem>
-                        ))}
-                    </ListboxSection>
-                </Listbox>
+                {isLoading ? (
+                    <Spinner />
+                ) : (
+                    <Listbox
+                        aria-label="Dynamic Actions"
+                        variant="solid"
+                        disallowEmptySelection={true}
+                        selectionMode="single"
+                        selectedKeys={selectedKeys}
+                        onSelectionChange={(keys) => {
+                            if (keys instanceof Set) {
+                                setSelectedKeys(keys as Set<string>);
+                            } else {
+                                setSelectedKeys(new Set([keys]));
+                            }
+                        }}
+                        autoFocus={true}
+                        itemClasses={{
+                            selectedIcon: "hide-icon"
+                        }}
+                    >
+                        <ListboxSection title="General" showDivider>
+                            {items.filter(item => item.section === "general").map(item => (
+                                <ListboxItem
+                                    key={item.key}
+                                    onPress={() => router.push(item.href)}
+                                >
+                                    {item.title}
+                                </ListboxItem>
+                            ))}
+                        </ListboxSection>
+                        <ListboxSection title="Contracts">
+                            {items.filter(item => item.section === "contracts").map(item => (
+                                <ListboxItem
+                                    key={item.key}
+                                    onPress={() => router.push(item.href)}
+                                >
+                                    {item.title}
+                                </ListboxItem>
+                            ))}
+                        </ListboxSection>
+                    </Listbox>
+                )}
             </div>
         </nav>
     );
