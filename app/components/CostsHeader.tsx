10a7702c5a391221b0629ce398757d818c800fdf
tvernier21 <thibaultvernier13@gmail.com>
2023-10-31 16:53:48 -0400

    compartmentalizing costs page

---
+++ app/components/CostsHeader.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useDisclosure } from '@nextui-org/react';
+import { FaFileInvoiceDollar } from 'react-icons/fa';
+
+import AddButton from './UI/AddButton';
+import CostModal from './modals/CostModal';
+
+const CostsHeader = () => {
+    const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
+
+    return (
+        <div className="flex justify-center items-center">
+            {/* <PropertyFilter /> */}
+            <AddButton
+                text="Add Cost/Renovation"
+                icon={FaFileInvoiceDollar}
+                onPressModal={onOpen}
+            />
+            <CostModal 
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                onClose={onClose}
+            />
+        </div>
+    );
+};
+
+export default CostsHeader;
--- app/components/CostsHome.tsx
+++ app/components/CostsHome.tsx
@@ -1,29 +1,14 @@
 'use client';
 
-import { useDisclosure } from '@nextui-org/react';
-import { FaFileInvoiceDollar } from 'react-icons/fa';
-
-import AddButton from './UI/AddButton';
-import CostModal from './modals/CostModal';
+import CostsHeader from './CostsHeader';
 
 const CostsHome = () => {
-    const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
-
     return (
-        <div className="flex justify-center items-center">
-            {/* <PropertyFilter /> */}
-            <AddButton
-                text="Add Cost/Renovation"
-                icon={FaFileInvoiceDollar}
-                onPressModal={onOpen}
-            />
-            <CostModal 
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-                onClose={onClose}
-            />
+        <div className="flex-col justify-center items-center space-y-10">
+            <CostsHeader />
+            {/* <CostsList /> */}
         </div>
-    );
+    )
 };
 
 export default CostsHome;
